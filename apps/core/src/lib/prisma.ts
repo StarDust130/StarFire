c744fe003a625cb0a3117f7d65f13e3fdf259a2f
StarDust130 <csyadav0513@gmail.com>
2026-05-17 13:49:09 +0530

    🔌 wire prisma adapter

--- apps/core/src/lib/prisma.ts
+++ apps/core/src/lib/prisma.ts
@@ -1,6 +1,11 @@
 import "dotenv/config";
 
-import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../../../../prisma/generated/client/client.js";
 
-export const prisma = new PrismaClient();
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
+
+export const prisma = new PrismaClient({ adapter });
 
