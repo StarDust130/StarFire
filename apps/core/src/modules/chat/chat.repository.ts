2da689beebc04d29e65cf7812b8b7dad64e3df0a
StarDust130 <csyadav0513@gmail.com>
2026-05-17 13:00:28 +0530

    feat: add chat repository with createMessage and getRecentMessages functions

---
+++ apps/core/src/modules/chat/chat.repository.ts
@@ -0,0 +1,38 @@
+import { Role } from "@prisma/client";
+import { prisma } from "../../lib/prisma.js";
+
+export async function createMessage(data: {
+  userId: string;
+  content: string;
+  role: Role;
+}) {
+  try {
+    console.log("DATA:", data);
+
+    const result = await prisma.message.create({
+      data,
+    });
+
+    return result;
+  } catch (error) {
+    console.error("FULL ERROR 👇");
+    console.dir(error, { depth: null });
+
+    throw error;
+  }
+}
+
+
+export async function getRecentMessages(userId: string) {
+  return prisma.message.findMany({
+    where: {
+      userId,
+    },
+
+    orderBy: {
+      createdAt: "desc",
+    },
+
+    take: 10,
+  });
+}
