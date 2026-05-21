11e9e2eb3ef0b506596dec472bc5d8beb087b0b6
StarDust130 <csyadav0513@gmail.com>
2026-05-21 16:07:25 +0530

    feat: import email-send.worker for email sending functionality

--- apps/workers/src/index.ts
+++ apps/workers/src/index.ts
@@ -2,5 +2,6 @@ import "dotenv/config";
 import { logger } from "../../core/src/lib/logger.js";
 import "./workers/memory.worker.js";
 import "./workers/email.worker.js";
+import "./workers/email-send.worker.js";
 
 logger.info("🐣 Workers Running...");
