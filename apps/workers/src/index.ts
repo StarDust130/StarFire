358ee6caaeba0ca9072a0b27fd19ac31e4be4b18
StarDust130 <csyadav0513@gmail.com>
2026-05-21 18:19:01 +0530

    feat: add resume worker import to initialize resume processing

--- apps/workers/src/index.ts
+++ apps/workers/src/index.ts
@@ -3,5 +3,6 @@ import { logger } from "../../core/src/lib/logger.js";
 import "./workers/memory.worker.js";
 import "./workers/email.worker.js";
 import "./workers/email-send.worker.js";
+import "./workers/resume.worker.js";
 
 logger.info("🐣 Workers Running...");
