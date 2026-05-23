6b77bf6d5a3b414bb6b50dc662fbc547dfe12c9e
StarDust130 <csyadav0513@gmail.com>
2026-05-23 23:14:52 +0530

    ✨ Integrate Clerk authentication components into RootLayout

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter, Playfair_Display } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider, Show, SignInButton, SignUpButton, UserButton ,  } from "@clerk/nextjs";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -27,7 +28,25 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${inter.variable} ${playfair.variable}`}>
-      <body className="font-sans antialiased">{children}</body>
+      <body className="font-sans antialiased">
+        {" "}
+        <ClerkProvider>
+          <header className="flex justify-end items-center p-4 gap-4 h-16">
+            <Show when="signed-out">
+              <SignInButton />
+              <SignUpButton>
+                <button className="bg-purple-700 text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                  Sign Up
+                </button>
+              </SignUpButton>
+            </Show>
+            <Show when="signed-in">
+              <UserButton />
+            </Show>
+          </header>
+          {children}
+        </ClerkProvider>
+      </body>
     </html>
   );
 }
