a41a3836df668be10097235bdc4cb7b09714dea9
StarDust130 <csyadav0513@gmail.com>
2026-05-24 09:42:59 +0530

    ✏️ layout.tsx

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter, Playfair_Display } from "next/font/google";
 import "./globals.css";
-import { ClerkProvider, Show, SignInButton, SignUpButton, UserButton ,  } from "@clerk/nextjs";
+import { ClerkProvider  } from "@clerk/nextjs";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -31,19 +31,6 @@ export default function RootLayout({
       <body className="font-sans antialiased">
         {" "}
         <ClerkProvider>
-          <header className="flex justify-end items-center p-4 gap-4 h-16">
-            <Show when="signed-out">
-              <SignInButton />
-              <SignUpButton>
-                <button className="bg-purple-700 text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-                  Sign Up
-                </button>
-              </SignUpButton>
-            </Show>
-            <Show when="signed-in">
-              <UserButton />
-            </Show>
-          </header>
           {children}
         </ClerkProvider>
       </body>
