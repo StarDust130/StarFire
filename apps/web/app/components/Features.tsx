5b56ca6d2f97ddd85fc20f9c0e52e1693b612c4b
StarDust130 <csyadav0513@gmail.com>
2026-05-29 11:40:38 +0530

    ✨ Update Features

--- apps/web/app/components/Features.tsx
+++ apps/web/app/components/Features.tsx
@@ -1,5 +1,5 @@
 import { Briefcase, Zap, Globe, Cpu, ArrowRight } from "lucide-react";
-import Button from "./Button";
+import Button from "./ui/Button";
 
 export default function Features() {
   return (
